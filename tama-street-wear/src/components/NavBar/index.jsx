940050a944aca7e7b08b357e6e19bba68bd96752
julianbischoff <julibischoff@gmail.com>
2023-05-31 03:11:10 -0600

    segundo_commit

---
+++ tama-street-wear/src/components/NavBar/index.jsx
@@ -0,0 +1,25 @@
+import "../NavBar/styles.scss";
+
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="container_global">
+      <ul className="left_items">
+        <li>Mens</li>
+        <li>Woman</li>
+        <li>SNKRS</li>
+        <li>Accessories</li>
+      </ul>
+      <div className="right_items">
+        <ul>
+          <li>Sale</li>
+          <li>About Us</li>
+        </ul>
+        <input type="text" placeholder="Search" className="search_bar" />
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
